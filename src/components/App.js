f8c81015ab595a2b27f763bf99fc09dd36565263
Samuele Maurizi <samuelemaurizi@gmail.com>
2020-01-28 13:02:33 +0100

    add features

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState, useRef, useEffect } from 'react';
+import uuidv4 from 'uuid/v4';
+import '../styles/App.css';
+
+import TodoList from './TodoList';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    todoNameRef.current.value = null;
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete);
+    setTodos(newTodos);
+  }
+
+  return (
+    <Fragment>
+      <input ref={todoNameRef} type='text'></input>
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Todo</button>
+      <div>
+        <span>{todos.filter(todo => !todo.complete).length}</span> left to do
+      </div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+    </Fragment>
+  );
+}
+
+export default App;
